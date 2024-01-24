08c01466425f2295d8216dc7f23f4269f5219a93
dan01jan <danize1001@gmail.com>
2024-01-24 16:53:30 +0800

    ProductContainer

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
+
+import ProductContainer from './Screens/Product/ProductContainer';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style = {styles.text}>NANDITO NA SI BONG REVILLAAA</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ProductContainer />
+  </View>
   );
 }
 
@@ -17,8 +19,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    fontFamily: 'Arial',
-    fontSize: 50,
-  }
 });
